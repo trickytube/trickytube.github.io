b14b8c1e73928be7ceacc003678adfa5d0a237a3
Tushar Manna <contact@tusharmanna.com>
2026-01-18 12:57:30 +0530

    implemented the whole siteconfig workflow on .env

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,8 @@
+const siteName = import.meta.env.PUBLIC_SITE_NAME;
+const siteDesc = import.meta.env.PUBLIC_SITE_DESCRIPTION
+
 
 export const siteConfig = {
-    name: "News Site",
-    description: "A news site",
+    name: siteName || "News Site",
+    description: siteDesc || "A news site",
 };
