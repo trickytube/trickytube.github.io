f06015d040e344dbb43b026c06c702259100dca3
Tushar Manna <contact@tusharmanna.com>
2026-03-16 12:34:42 +0530

    added opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import {loadEnv} from "vite";
+
+const { PUBLIC_SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
+  site:PUBLIC_SITE_URL,
   integrations: [partytown(), sitemap()],
 
   vite: {
